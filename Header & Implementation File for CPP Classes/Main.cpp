8cfbfc0123717ac71c62701977751883d60c8a4d
Kulpot <kulpotwahu@gmail.com>
2024-06-01 03:34:33 +0800

    START:

--- Header & Implementation File for CPP Classes/Main.cpp
+++ Header & Implementation File for CPP Classes/Main.cpp
@@ -1,4 +1,3 @@
-
 //Header & Implementation File for C++ Classes | C++ Tutorial  //ref link:https://www.youtube.com/watch?v=ru_u9N7jydU&list=PLFk1_lkqT8MaImkuU0ArJ6_dbr9nEN46a&index=4
 
 class Monster {
