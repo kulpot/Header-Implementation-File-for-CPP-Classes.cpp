fc770ddcb9de0c91fa00f1c743974f42f444e2ac
Kulpot <kulpotwahu@gmail.com>
2024-06-01 03:32:31 +0800

    class Monster { public:  Monster(int hp) {   mHp = hp;  }
    private:
     int mHp;
    };
    
    int main() {
     return 0;

--- Header & Implementation File for CPP Classes/Main.cpp
+++ Header & Implementation File for CPP Classes/Main.cpp
@@ -1,3 +1,17 @@
 
 //Header & Implementation File for C++ Classes | C++ Tutorial  //ref link:https://www.youtube.com/watch?v=ru_u9N7jydU&list=PLFk1_lkqT8MaImkuU0ArJ6_dbr9nEN46a&index=4
 
+class Monster {
+public:
+	Monster(int hp) {
+		mHp = hp;
+	}
+
+
+private:
+	int mHp;
+};
+
+int main() {
+	return 0;
+}
