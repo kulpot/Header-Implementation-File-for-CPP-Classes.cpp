0c6e878a46d62de394497d5478acd432e05cafb0
Kulpot <kulpotwahu@gmail.com>
2024-06-02 00:26:20 +0800

    ..

--- Header & Implementation File for CPP Classes/Main.cpp
+++ Header & Implementation File for CPP Classes/Main.cpp
@@ -1,26 +1,26 @@
 //Header & Implementation File for C++ Classes | C++ Tutorial  //ref link:https://www.youtube.com/watch?v=ru_u9N7jydU&list=PLFk1_lkqT8MaImkuU0ArJ6_dbr9nEN46a&index=4
 
-class Monster {
-public:
-	Monster(int hp) {
-		mHp = hp;
-	}
-
-	int getHp() {
-		return mHp;
-	}
-
-	void takeDamage(int amount) {
-		mHp -= amount;
-
-		if (mHp < 0) {
-			mHp = 0;
-		}
-	}
-
-private:
-	int mHp;
-};
+//class Monster {
+//public:
+//	Monster(int hp) {
+//		mHp = hp;
+//	}
+//
+//	int getHp() {
+//		return mHp;
+//	}
+//
+//	void takeDamage(int amount) {
+//		mHp -= amount;
+//
+//		if (mHp < 0) {
+//			mHp = 0;
+//		}
+//	}
+//
+//private:
+//	int mHp;
+//};
 
 int main() {
 	return 0;
